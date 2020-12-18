75cdf6b68c9077f32ee208ffcec2750248bb837d
Shyn <56405907+Shynltd@users.noreply.github.com>
2020-12-19 00:31:33 +0700

    1

--- app/src/main/java/com/example/thinw/MainActivity.java
+++ app/src/main/java/com/example/thinw/MainActivity.java
@@ -6,10 +6,26 @@ import androidx.constraintlayout.solver.state.State;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String LINK = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AndroidNetworking.initialize(getApplicationContext());
+        AndroidNetworking.setParserFactory(new JacksonParserFactory());
+        AndroidNetworking.get(LINK)
+                .addPathParameter("pageNumber", "0")
+                .addQueryParameter("limit", "3")
+                .build()
+                .getAsJSONArray(new JSONArrayRequestListener() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+                    }
+                    @Override
+                    public void onError(ANError error) {
+
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/thinw/MyAdapter.java
+++ app/src/main/java/com/example/thinw/MyAdapter.java
@@ -1,4 +1,44 @@
 package com.example.thinw;
 
-public class MyAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
+    private Context context;
+    private List<String> mList;
+
+    public MyAdapter(Context context, List<String> mList) {
+        this.context = context;
+        this.mList = mList;
+    }
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.rv_list, parent, false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList.size();
+    }
+
+    public static class MyHolder extends RecyclerView.ViewHolder {
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
