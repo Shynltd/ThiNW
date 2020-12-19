818cfb26b32f8f5e07da6782b2284a7d77b82213
Shyn <56405907+Shynltd@users.noreply.github.com>
2020-12-19 08:03:02 +0700

    q

--- app/src/main/java/com/example/thinw/MainActivity.java
+++ app/src/main/java/com/example/thinw/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.thinw;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.solver.state.State;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.error.ANError;
@@ -11,27 +15,66 @@ import com.androidnetworking.interfaces.JSONArrayRequestListener;
 import com.jacksonandroidnetworking.JacksonParserFactory;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private String LINK = "";
+    private String LINK = "https://jsonplaceholder.typicode.com/comments";
+    private TextView tvSearch;
+    private RecyclerView rvList;
+    private MyAdapter myAdapter;
+    private List<Example> exampleList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+        exampleList = new ArrayList<>();
+        myAdapter = new MyAdapter(MainActivity.this,exampleList);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
+        rvList.setLayoutManager(linearLayoutManager);
+        rvList.setHasFixedSize(true);
+        rvList.setAdapter(myAdapter);
+        getEmail();
+
+    }
+
+    private void getEmail() {
         AndroidNetworking.initialize(getApplicationContext());
         AndroidNetworking.setParserFactory(new JacksonParserFactory());
         AndroidNetworking.get(LINK)
-                .addPathParameter("pageNumber", "0")
                 .build()
                 .getAsJSONArray(new JSONArrayRequestListener() {
                     @Override
                     public void onResponse(JSONArray response) {
-
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                Integer postId = response.getJSONObject(i).getInt("postId");
+                                Integer id = response.getJSONObject(i).getInt("id");
+                                String name = response.getJSONObject(i).getString("name");
+                                String email = response.getJSONObject(i).getString("email");
+                                String body = response.getJSONObject(i).getString("body");
+                                exampleList.add(new Example(postId,id,name,email,body));
+                                myAdapter.notifyDataSetChanged();
+                            }
+                        } catch (JSONException e) {
+                            Log.e("onResponse: ", e.getMessage() );
+                        }
                     }
+
                     @Override
                     public void onError(ANError error) {
 
+                        Log.e("error: ", error.getMessage() );
                     }
                 });
+    }
+
+    private void initView() {
+        tvSearch = (TextView) findViewById(R.id.tvSearch);
+        rvList = (RecyclerView) findViewById(R.id.rvList);
     }
 }
--- app/src/main/java/com/example/thinw/MyAdapter.java
+++ app/src/main/java/com/example/thinw/MyAdapter.java
@@ -1,9 +1,13 @@
 package com.example.thinw;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,9 +16,9 @@ import java.util.List;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
     private Context context;
-    private List<String> mList;
+    private List<Example> mList;
 
-    public MyAdapter(Context context, List<String> mList) {
+    public MyAdapter(Context context, List<Example> mList) {
         this.context = context;
         this.mList = mList;
     }
@@ -28,7 +32,32 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
-
+        holder.tvComment.setText(mList.get(position).getEmail());
+        for (int i=0;i<mList.get(position).getEmail().length();i++){
+            String n = String.valueOf(mList.get(position).getEmail().charAt(i));
+            if (n.equalsIgnoreCase("n")) {
+                holder.tvComment.setTextColor(Color.parseColor("#2C367A"));
+            };
+        }
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                View alert = LayoutInflater.from(context).inflate(R.layout.dialog,null);
+                builder.setView(alert);
+                TextView tvPostId = alert.findViewById(R.id.tvPostId);
+                TextView tvName= alert.findViewById(R.id.tvName);
+                TextView tvId= alert.findViewById(R.id.tvId);
+                TextView tvbody= alert.findViewById(R.id.tvBody);
+                TextView tvEmail= alert.findViewById(R.id.tvEmail);
+                tvPostId.setText(mList.get(position).getPostId()+"");
+                tvName.setText(mList.get(position).getName()+"");
+                tvId.setText(mList.get(position).getId()+"");
+                tvbody.setText(mList.get(position).getBody()+"");
+                tvEmail.setText(mList.get(position).getEmail()+"");
+                builder.create().show();
+            }
+        });
     }
 
     @Override
@@ -37,8 +66,10 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
     }
 
     public static class MyHolder extends RecyclerView.ViewHolder {
+        TextView tvComment;
         public MyHolder(@NonNull View itemView) {
             super(itemView);
+            tvComment = itemView.findViewById(R.id.tvComment);
         }
     }
 }
